b83e200abc1453edafdc2471f135119fe9e3a7f4
igorzobov <igor_zobov@mail.ru>
2021-09-15 19:42:31 +0700

    connetct DB of jdbc and check values

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,36 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+
+;import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Objects;
+
 public class Main {
+
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        User user = new User();
+        try {
+            Statement statement = Objects.requireNonNull(Util.getConnectionJDBC()).createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM users");
+
+            while (resultSet.next()) {
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setLastName(resultSet.getString("lastName"));
+                user.setAge(resultSet.getByte("age"));
+                System.out.println("id: " + user.getId());
+                System.out.println("name: " + user.getName());
+                System.out.println("lastName: " + user.getLastName());
+                System.out.println("age: " + user.getAge());
+            }
+            System.out.println(user.getId());
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,28 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.*;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://127.0.0.1/usersdb?useUnicode=true&serverTimezone=UTC&useSSL=false";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
+    public static Connection getConnectionJDBC() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+//            Statement statement = connection.createStatement();
+//            ResultSet resultSet = statement.executeQuery("SELECT * FROM users");
+//
+//            while (resultSet.next()) {
+//                String userName = resultSet.getString("name");
+//                System.out.println(userName);
+//            }
+//            resultSet.close();
+//            statement.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
